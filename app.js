7c97c06e88d9e83c8c3b5579bd1ed903c676e0a2
Naoto31 <naotosai@Naotos-MBP.fritz.box>
2020-04-23 09:20:38 +0200

    Update

--- app.js
+++ app.js
@@ -53,10 +53,12 @@ addPhraseToDisplay(phraseArray);
 qwerty.addEventListener('click', e => {
     if (e.target.tagName === 'BUTTON') {
         const buttonLetter = e.target.textContent;
-        const match = checkLetter(buttonLetter);
+        const buttonElement = e.target;
+        const match = checkLetter(buttonLetter, buttonElement);
         if (!match) {
             tries[missNumber].style.display = 'none';
             missNumber += 1;
+            buttonElement.classList.add('chosen_fail');
 
             // Lose condition
             if (missNumber >= 5) {
@@ -65,8 +67,9 @@ qwerty.addEventListener('click', e => {
                 const div = document.getElementById('overlay');
                 const tryButton = document.getElementsByClassName('btn__reset');
                 div.appendChild(h3);
+                div.classList.add('lose');
                 h3.innerHTML = 'You Lose!';
-                for (var i = 0; i < tryButton.length; i += 1) {
+                for (let i = 0; i < tryButton.length; i += 1) {
                     tryButton[i].innerHTML = 'Try again';
                 }
             }
@@ -74,42 +77,78 @@ qwerty.addEventListener('click', e => {
     }
 });
 
+// Get a letter from the keyboard and pass to the checkLetter function
+
+document.addEventListener('keydown', function (event) {
+        const buttonLetter = event.key;
+        const buttonTags = document.getElementsByTagName('button');
+        const searchLetter = buttonLetter;
+        var found;
+        
+        for (let i = 0; i < buttonTags.length; i += 1) {
+            if (buttonTags[i].textContent == searchLetter) {
+                found = buttonTags[i];
+                break;
+              }
+        }
+        const buttonElement = found;
+        const match = checkLetter(buttonLetter, buttonElement);
+
+        if (!match) {
+            tries[missNumber].style.display = 'none';
+            missNumber += 1;
+            buttonElement.classList.add('chosen_fail');
+
+            // Lose condition
+            if (missNumber >= 5) {
+                overlay.style.display = '';
+                const h3 = document.createElement('h3');
+                const div = document.getElementById('overlay');
+                const tryButton = document.getElementsByClassName('btn__reset');
+                div.appendChild(h3);
+                div.classList.add('lose');
+                h3.innerHTML = 'You Lose!';
+                for (let i = 0; i < tryButton.length; i += 1) {
+                    tryButton[i].innerHTML = 'Try again';
+                }
+            }
+        }
+    
+});
+
+
 // Create a checkLetter function
 
-function checkLetter(buttonLetter) {
+function checkLetter(buttonLetter, buttonElement) {
     letterFound = false;
     const phrase = document.querySelectorAll('.letter');
     for (let i = 0; i < phrase.length; i += 1) {
         const letter = phrase[i];
         if (buttonLetter === letter.textContent.toLowerCase()) {
-            letter.classList.add('show', 'chosen');
+            letter.classList.add('show');
+            buttonElement.classList.add('chosen');
             letterFound = true;
 
             // Win condition
             const ul = document.getElementsByTagName('ul')[0];
             const liWithClassShow = ul.getElementsByClassName('show');
             const letterLength = liWithClassShow.length;
+
             if (letterLength === phrase.length) {
                 overlay.style.display = '';
                 const h3 = document.createElement('h3');
                 const div = document.getElementById('overlay');
+                const againButton = document.getElementsByClassName('btn__reset');
+                div.classList.add('win');
                 div.appendChild(h3);
                 h3.innerHTML = 'You Win!';
-
-
+                for (let i = 0; i < againButton.length; i += 1) {
+                    againButton[i].innerHTML = 'Play again';
+                }
 
             }
+
         }
     }
     return letterFound;
 }
-
-
-
-
-// Get a letter from the keyboard and pass to the checkLetter function
-
-document.addEventListener('keydown', function (event) {
-    checkLetter(event.key);
-
-});
